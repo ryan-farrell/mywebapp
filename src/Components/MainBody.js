aea8a0872252653eafe94cdd8f444ac3e5481a66
ryan-farrell <ryan.farrell44@gmail.com>
2019-06-04 00:07:13 +0100

    Picture a sizing complete. New font added

--- src/Components/MainBody.js
+++ src/Components/MainBody.js
@@ -8,6 +8,10 @@ const profile2 = require('../img/profile.jpg')
 
 
 
+
+
+
+
 const MainBody = () => {
         return (
         <div className = "header">
@@ -15,19 +19,23 @@ const MainBody = () => {
             <img alt = "Profile" src = {profile2} className = "frontImage"></img>
             <img alt = "" src = {profile} className = "backImage"></img>
             
+            <div className = "textBox">
+            <p>Currently attending a Coding Bootcamp @ Code Nation. Looking to graduate early July'19 versed and had exposure to a modern day tech stack such as JavaScript, React, Node.js, MongoDB, Swift, mySQL. Using differing methodologies like Waterfall and Agile with lightning talks and stand ups throughout my course. As well as improving my new developing skills daily also using other tech accessories to aid in the above with the likes of Git, Github, Slack, Toggl & Trello.</p>
             
-            <p>Code Nation Text to go here</p>
             <ul>
                 
-            <li>Email</li>
-            <li>Mobile</li>
-            <li>CodeNation www</li>
-            <li>Manchester, UK</li>
-
-             
+            <email>ryan.farrell44@gmail.com</email><br>
+            </br>
+            <number>07921 237 182</number><br>
+            </br>
+            <location>Manchester, UK</location><br>
+            </br>
+            <url>https://wearecodenation.com/</url>
+            
              
                 </ul>
-            
+                
+                </div>
 
         </div>
                   
